a3f3c03c78eedc811960995f9b45435194897ff5
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-05-27 22:56:21 +0300

    Loop test

--- tests/test_consoleapi.cc
+++ tests/test_consoleapi.cc
@@ -8,17 +8,19 @@ namespace wd = winapi::detail;
 
 TEST(ConsoleAPI, ConsoleHost)
 {
-  try {
-    auto host = winapi::ConsoleHost(winapi::ConsoleHost::Mode::All);
+  for(auto i = 0; i < 2; ++i) {
+    try {
+      auto host = winapi::ConsoleHost(winapi::ConsoleHost::Mode::All);
 
-    EXPECT_TRUE(host.is_attached());
-    EXPECT_NO_THROW(host.set_title("winapi20 test console host"));
-    EXPECT_EQ(host.title(), "winapi20 test console host");
+      EXPECT_TRUE(host.is_attached());
+      EXPECT_NO_THROW(host.set_title("winapi20 test console host"));
+      EXPECT_EQ(host.title(), "winapi20 test console host");
 
-    cout << "Test is ok" << endl;
-    cin.get();
-  } catch(winapi::windows_exception const& e) {
-    GTEST_FATAL_FAILURE_(e.what());
+      cout << "Test is ok" << i << endl;
+      cin.get();
+    } catch(winapi::windows_exception const& e) {
+      GTEST_FATAL_FAILURE_(e.what());
+    }
   }
   GTEST_SUCCEED();
 }
